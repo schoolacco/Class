152aeb2c37ad910382e89c1ecf644aaa4d745df5
schoolacco <127917983+schoolacco@users.noreply.github.com>
2025-05-02 10:13:31 +1000

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,7 +1,11 @@
 from subjects import Subject
 print('''Welcome to the subjects manager.
 --------------------------------''')
-input('''Commands:
+while True:
+  choice = input('''Commands:
       1. Add a subject to the list
       2. View a chosen subject from the list
-      3. View all subject details from the list''')
+      3. View all subject details from the list
+      Insert your command (number): ''')
+  if choice == '1':
+    subject = Subject()
